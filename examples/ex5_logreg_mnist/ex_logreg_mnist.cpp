40c5d054d0506f8d3f38edbfe1cceb318f346b88
koosy <koosy@ais.uni-bonn.de>
2016-01-07 20:37:21 +0100

    _ deleted

--- examples/ex5_logreg_mnist/ex_logreg_mnist.cpp
+++ examples/ex5_logreg_mnist/ex_logreg_mnist.cpp
@@ -20,13 +20,13 @@ int main()
     Caffe::set_mode(Caffe::GPU);
 
     // data layer
-    vector<Blob<Dtype>*> blob_bottom_data_vec_;
-    vector<Blob<Dtype>*> blob_top_data_vec_;
+    vector<Blob<Dtype>*> blob_bottom_data_vec;
+    vector<Blob<Dtype>*> blob_top_data_vec;
     Blob<Dtype>* const blob_data = new Blob<Dtype>();
     Blob<Dtype>* const blob_label = new Blob<Dtype>();
 
-    blob_top_data_vec_.push_back(blob_data);
-    blob_top_data_vec_.push_back(blob_label);
+    blob_top_data_vec.push_back(blob_data);
+    blob_top_data_vec.push_back(blob_label);
 
     LayerParameter layer_data_param;
     DataParameter* data_param = layer_data_param.mutable_data_param();
@@ -38,15 +38,15 @@ int main()
     transform_param->set_scale(1./255.);
 
     DataLayer<Dtype> layer_data(layer_data_param);
-    layer_data.SetUp(blob_bottom_data_vec_, blob_top_data_vec_);
+    layer_data.SetUp(blob_bottom_data_vec, blob_top_data_vec);
 
     //set inner product layer
-    vector<Blob<Dtype>*> blob_bottom_ip_vec_;
-    vector<Blob<Dtype>*> blob_top_ip_vec_;
-    Blob<Dtype>* const blob_top_ip_ = new Blob<Dtype>();
+    vector<Blob<Dtype>*> blob_bottom_ip_vec;
+    vector<Blob<Dtype>*> blob_top_ip_vec;
+    Blob<Dtype>* const blob_top_ip = new Blob<Dtype>();
 
-    blob_bottom_ip_vec_.push_back(blob_data);
-    blob_top_ip_vec_.push_back(blob_top_ip_);
+    blob_bottom_ip_vec.push_back(blob_data);
+    blob_top_ip_vec.push_back(blob_top_ip);
 
     LayerParameter layer_ip_param;
     layer_ip_param.mutable_inner_product_param()->set_num_output(10);
@@ -54,36 +54,36 @@ int main()
     layer_ip_param.mutable_inner_product_param()->mutable_bias_filler()->set_type("constant");
 
     InnerProductLayer<Dtype> layer_ip(layer_ip_param);
-    layer_ip.SetUp(blob_bottom_ip_vec_, blob_top_ip_vec_);
+    layer_ip.SetUp(blob_bottom_ip_vec, blob_top_ip_vec);
 
     // set softmax loss layer
-    vector<Blob<Dtype>*> blob_bottom_loss_vec_;
-    vector<Blob<Dtype>*> blob_top_loss_vec_;
-    Blob<Dtype>* const blob_top_loss_ = new Blob<Dtype>();
+    vector<Blob<Dtype>*> blob_bottom_loss_vec;
+    vector<Blob<Dtype>*> blob_top_loss_vec;
+    Blob<Dtype>* const blob_top_loss = new Blob<Dtype>();
 
-    blob_bottom_loss_vec_.push_back(blob_top_ip_);
-    blob_bottom_loss_vec_.push_back(blob_label);
-    blob_top_loss_vec_.push_back(blob_top_loss_);
+    blob_bottom_loss_vec.push_back(blob_top_ip);
+    blob_bottom_loss_vec.push_back(blob_label);
+    blob_top_loss_vec.push_back(blob_top_loss);
 
     LayerParameter layer_loss_param;
     SoftmaxWithLossLayer<Dtype> layer_loss(layer_loss_param);
-    layer_loss.SetUp(blob_bottom_loss_vec_, blob_top_loss_vec_);\
+    layer_loss.SetUp(blob_bottom_loss_vec, blob_top_loss_vec);\
 
 
     // forward and backward iteration
     for(int n=0;n<nIter;n++){
         // forward
-        layer_data.Forward(blob_bottom_data_vec_, blob_top_data_vec_);
-        layer_ip.Forward(blob_bottom_ip_vec_, blob_top_ip_vec_);
-        Dtype loss = layer_loss.Forward(blob_bottom_loss_vec_, blob_top_loss_vec_);
+        layer_data.Forward(blob_bottom_data_vec, blob_top_data_vec);
+        layer_ip.Forward(blob_bottom_ip_vec, blob_top_ip_vec);
+        Dtype loss = layer_loss.Forward(blob_bottom_loss_vec, blob_top_loss_vec);
         cout<<"Iter "<<n<<" loss "<<loss<<endl;
 
         // backward
         vector<bool> backpro_vec;
         backpro_vec.push_back(1);
         backpro_vec.push_back(0);
-        layer_loss.Backward(blob_top_loss_vec_, backpro_vec, blob_bottom_loss_vec_);
-        layer_ip.Backward(blob_top_ip_vec_, backpro_vec, blob_bottom_ip_vec_);
+        layer_loss.Backward(blob_top_loss_vec, backpro_vec, blob_bottom_loss_vec);
+        layer_ip.Backward(blob_top_ip_vec, backpro_vec, blob_bottom_ip_vec);
 
         // update weights of layer_ip
         Dtype rate = 0.1;
@@ -95,13 +95,13 @@ int main()
 
     //prediction
     // data layer
-    vector<Blob<Dtype>*> blob_bottom_testdata_vec_;
-    vector<Blob<Dtype>*> blob_top_testdata_vec_;
+    vector<Blob<Dtype>*> blob_bottom_testdata_vec;
+    vector<Blob<Dtype>*> blob_top_testdata_vec;
     Blob<Dtype>* const blob_testdata = new Blob<Dtype>();
     Blob<Dtype>* const blob_testlabel = new Blob<Dtype>();
 
-    blob_top_testdata_vec_.push_back(blob_testdata);
-    blob_top_testdata_vec_.push_back(blob_testlabel);
+    blob_top_testdata_vec.push_back(blob_testdata);
+    blob_top_testdata_vec.push_back(blob_testlabel);
 
     LayerParameter layer_testdata_param;
     DataParameter* testdata_param = layer_testdata_param.mutable_data_param();
@@ -113,24 +113,24 @@ int main()
     transform_test_param->set_scale(1./255.);
 
     DataLayer<Dtype> layer_testdata(layer_testdata_param);
-    layer_testdata.SetUp(blob_bottom_testdata_vec_, blob_top_testdata_vec_);
+    layer_testdata.SetUp(blob_bottom_testdata_vec, blob_top_testdata_vec);
 
-    vector<Blob<Dtype>*> blob_bottom_ip_test_vec_;
-    blob_bottom_ip_test_vec_.push_back(blob_testdata);
+    vector<Blob<Dtype>*> blob_bottom_ip_test_vec;
+    blob_bottom_ip_test_vec.push_back(blob_testdata);
 
-    layer_ip.Reshape(blob_bottom_ip_test_vec_, blob_top_ip_vec_);
+    layer_ip.Reshape(blob_bottom_ip_test_vec, blob_top_ip_vec);
 
-    vector<Blob<Dtype>*> blob_bottom_loss_test_vec_;
-    blob_bottom_loss_test_vec_.push_back(blob_top_ip_);
-    blob_bottom_loss_test_vec_.push_back(blob_testlabel);
+    vector<Blob<Dtype>*> blob_bottom_loss_test_vec;
+    blob_bottom_loss_test_vec.push_back(blob_top_ip);
+    blob_bottom_loss_test_vec.push_back(blob_testlabel);
 
-    layer_loss.Reshape(blob_bottom_loss_test_vec_, blob_top_loss_vec_);
+    layer_loss.Reshape(blob_bottom_loss_test_vec, blob_top_loss_vec);
 
     // armax layer
     vector<Blob<Dtype>*> blob_bottom_argmax_vec;
     vector<Blob<Dtype>*> blob_top_argmax_vec;
     Blob<Dtype>* blob_top_argmax = new Blob<Dtype>();
-    blob_bottom_argmax_vec.push_back(blob_top_ip_);
+    blob_bottom_argmax_vec.push_back(blob_top_ip);
     blob_top_argmax_vec.push_back(blob_top_argmax);
 
     LayerParameter layer_argmax_param;
@@ -143,11 +143,11 @@ int main()
     int correct = 0;
     int cnt = 0;
     // forward
-    layer_testdata.Forward(blob_bottom_testdata_vec_, blob_top_testdata_vec_);
-    layer_ip.Forward(blob_bottom_ip_test_vec_, blob_top_ip_vec_);
+    layer_testdata.Forward(blob_bottom_testdata_vec, blob_top_testdata_vec);
+    layer_ip.Forward(blob_bottom_ip_test_vec, blob_top_ip_vec);
     layer_argmax.Forward(blob_bottom_argmax_vec, blob_top_argmax_vec);
 
-    Dtype loss = layer_loss.Forward(blob_bottom_loss_test_vec_, blob_top_loss_vec_);
+    Dtype loss = layer_loss.Forward(blob_bottom_loss_test_vec, blob_top_loss_vec);
     cout<<" loss "<<loss<<endl;
 
     for (int n = 0; n<blob_testlabel->count();n++){  // 10 <<-- num_test
